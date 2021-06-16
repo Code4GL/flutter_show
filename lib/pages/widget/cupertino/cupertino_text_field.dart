b9db158b6648110b9b29ae5b1eb5c24c8a8f2d44
Code4GL <guanli1991@163.com>
2021-06-16 10:10:10 +0800

    Cupertino组件完成

--- lib/pages/widget/cupertino/cupertino_text_field.dart
+++ lib/pages/widget/cupertino/cupertino_text_field.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_show/common/my_style.dart';
+import 'package:flutter_show/components/boolean_param.dart';
+import 'package:flutter_show/components/radio_param.dart';
 
 class CupertinoTextFieldPage extends StatefulWidget {
   @override
@@ -7,6 +10,22 @@ class CupertinoTextFieldPage extends StatefulWidget {
 }
 
 class _CupertinoTextFieldPageState extends State<CupertinoTextFieldPage> {
+  TextEditingController _textController;
+  bool _showCursor = true;
+  Color _cursorColor = Colors.blue;
+  double _cursorHeight = 20.0;
+  double _cursorWidth = 2.0;
+  bool _enabled = true;
+  bool _readOnly = false;
+  int _maxLines = 1;
+  int _minLines = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _textController = TextEditingController(text: 'initial text');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -32,7 +51,7 @@ class _CupertinoTextFieldPageState extends State<CupertinoTextFieldPage> {
                 ),
               ),
               Text(
-                '',
+                '这个小部件对应于iOS上的UITextField和可编辑的UITextView。在查找某些用户输入的长度时，使用string.characters.length。不要使用string.length甚至string.runes.length。对于复杂字符“👨‍👩‍👦”，这在用户看来是一个单一的字符，string.characters.length直观地返回1。另一方面，string.length返回8，string.runes.length返回5！',
                 style: TextStyle(
                   fontSize: MyStyle.scenesContentFontSize,
                   color: MyStyle.scenesContentColor,
@@ -59,6 +78,207 @@ class _CupertinoTextFieldPageState extends State<CupertinoTextFieldPage> {
                   fontWeight: MyStyle.titleFontWeight,
                 ),
               ),
+              BooleanParam(
+                paramKey: 'showCursor:',
+                paramValue: '$_showCursor',
+                value: _showCursor,
+                onChangedCb: (bool value) {
+                  setState(() {
+                    _showCursor = value;
+                  });
+                },
+              ),
+              RadioParam(
+                paramKey: 'cursorColor:',
+                paramValue:
+                    '#${_cursorColor.value.toRadixString(16).toUpperCase()}',
+                groupValue: _cursorColor,
+                items: [
+                  {
+                    'name': 'blue',
+                    'value': Colors.blue,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _cursorColor = value;
+                      });
+                    },
+                  },
+                  {
+                    'name': 'brown',
+                    'value': Colors.brown,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _cursorColor = value;
+                      });
+                    },
+                  },
+                  {
+                    'name': 'orange',
+                    'value': Colors.orange,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _cursorColor = value;
+                      });
+                    },
+                  },
+                ],
+              ),
+              RadioParam(
+                paramKey: 'cursorHeight:',
+                paramValue: '$_cursorHeight',
+                groupValue: _cursorHeight,
+                items: [
+                  {
+                    'name': '20.0',
+                    'value': 20.0,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _cursorHeight = value;
+                      });
+                    },
+                  },
+                  {
+                    'name': '30.0',
+                    'value': 30.0,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _cursorHeight = value;
+                      });
+                    },
+                  },
+                  {
+                    'name': '40.0',
+                    'value': 40.0,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _cursorHeight = value;
+                      });
+                    },
+                  },
+                ],
+              ),
+              RadioParam(
+                paramKey: 'cursorWidth:',
+                paramValue: '$_cursorWidth',
+                groupValue: _cursorWidth,
+                items: [
+                  {
+                    'name': '2.0',
+                    'value': 2.0,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _cursorWidth = value;
+                      });
+                    },
+                  },
+                  {
+                    'name': '3.0',
+                    'value': 3.0,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _cursorWidth = value;
+                      });
+                    },
+                  },
+                  {
+                    'name': '4.0',
+                    'value': 4.0,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _cursorWidth = value;
+                      });
+                    },
+                  },
+                ],
+              ),
+              BooleanParam(
+                paramKey: 'enabled:',
+                paramValue: '$_enabled',
+                value: _enabled,
+                onChangedCb: (bool value) {
+                  setState(() {
+                    _enabled = value;
+                  });
+                },
+              ),
+              BooleanParam(
+                paramKey: 'readOnly:',
+                paramValue: '$_readOnly',
+                value: _readOnly,
+                onChangedCb: (bool value) {
+                  setState(() {
+                    _readOnly = value;
+                  });
+                },
+              ),
+              RadioParam(
+                paramKey: 'maxLines:',
+                paramValue: '$_maxLines',
+                groupValue: _maxLines,
+                items: [
+                  {
+                    'name': '3',
+                    'value': 3,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _maxLines = value;
+                      });
+                    },
+                  },
+                  {
+                    'name': '4',
+                    'value': 4,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _maxLines = value;
+                      });
+                    },
+                  },
+                  {
+                    'name': '5',
+                    'value': 5,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _maxLines = value;
+                      });
+                    },
+                  },
+                ],
+              ),
+              RadioParam(
+                paramKey: 'minLines:',
+                paramValue: '$_minLines',
+                groupValue: _minLines,
+                items: [
+                  {
+                    'name': '1',
+                    'value': 1,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _minLines = value;
+                      });
+                    },
+                  },
+                  {
+                    'name': '2',
+                    'value': 2,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _minLines = value;
+                      });
+                    },
+                  },
+                  {
+                    'name': '3',
+                    'value': 3,
+                    'onChangedCb': (value) {
+                      setState(() {
+                        _minLines = value;
+                      });
+                    },
+                  },
+                ],
+              ),
             ],
           ),
         ),
@@ -76,7 +296,19 @@ class _CupertinoTextFieldPageState extends State<CupertinoTextFieldPage> {
             ],
             borderRadius: MyStyle.borderRadius,
           ),
-          child: Center(),
+          child: Center(
+            child: CupertinoTextField(
+              controller: _textController,
+              showCursor: _showCursor,
+              cursorColor: _cursorColor,
+              cursorHeight: _cursorHeight,
+              cursorWidth: _cursorWidth,
+              enabled: _enabled,
+              readOnly: _readOnly,
+              maxLines: _maxLines,
+              minLines: _minLines,
+            ),
+          ),
         ),
       ],
     );
