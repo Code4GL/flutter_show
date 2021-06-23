ec273a01d5478cb0d8a3af4eb88dc3523472718c
Code4GL <guanli1991@163.com>
2021-06-23 15:45:01 +0800

    package_cell添加

---
+++ lib/components/package_cell.dart
@@ -0,0 +1,257 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_show/provider/language_provider.dart';
+
+class PackageCell extends StatelessWidget {
+  PackageCell({
+    Key key,
+    this.title,
+    this.introductionEN,
+    this.introductionCN,
+    this.routeName,
+    this.isNullSafety,
+    this.isFavourite,
+    this.owner,
+    this.ownerPath,
+    this.detailPath,
+    this.flutter,
+    this.dart,
+  }) : super(key: key);
+  final String title;
+  final String introductionEN;
+  final String introductionCN;
+  final String routeName;
+  final bool isNullSafety;
+  final bool isFavourite;
+  final String owner;
+  final String ownerPath;
+  final String detailPath;
+  final List flutter;
+  final List dart;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: (BuildContext context) {
+        //       return SecondaryWidgetPage(
+        //         title: this.title,
+        //         fileName: this.fileName,
+        //       );
+        //     },
+        //   ),
+        // );
+      },
+      child: Container(
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 5),
+        alignment: AlignmentDirectional.centerStart,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(3, 5),
+              blurRadius: 3,
+              spreadRadius: 3,
+              color: Colors.grey[300],
+            ),
+          ],
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              alignment: AlignmentDirectional.centerStart,
+              child: Text(
+                "$title",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue[800],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 5, bottom: 5),
+              alignment: AlignmentDirectional.centerStart,
+              child: Consumer<LanguageProvider>(
+                builder: (context, LanguageProvider languageProvider, _) =>
+                    Text(
+                  languageProvider.lang == "zh"
+                      ? "$introductionCN"
+                      : "$introductionEN",
+                  style: TextStyle(color: Colors.black45),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 5, bottom: 5),
+              child: Row(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image(
+                        height: 12,
+                        width: 12,
+                        color: Colors.grey[600],
+                        image: AssetImage(
+                          'assets/images/package/verified-publisher-icon.png',
+                        ),
+                      ),
+                      Text(
+                        "$owner",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 11,
+                          color: Colors.blue[800],
+                        ),
+                      ),
+                    ],
+                  ),
+                  isFavourite
+                      ? Container(
+                          margin: EdgeInsets.only(left: 5),
+                          padding: EdgeInsets.fromLTRB(5, 1, 5, 2),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue[800]),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            children: [
+                              FlutterLogo(size: 10),
+                              Text(
+                                'Flutter Favorite',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Colors.blue[800],
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      : Container(),
+                  isNullSafety
+                      ? Container(
+                          margin: EdgeInsets.only(left: 5),
+                          padding: EdgeInsets.fromLTRB(5, 1, 5, 2),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue[800]),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Text(
+                            'Null Safety',
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.blue[800],
+                            ),
+                          ),
+                        )
+                      : Container(),
+                ],
+              ),
+            ),
+            flutter.length > 0
+                ? Container(
+                    margin: EdgeInsets.only(top: 5),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: Colors.lightBlue[50],
+                            border: Border(
+                              right: BorderSide(
+                                width: 0.5,
+                                color: Colors.blue[800],
+                              ),
+                            ),
+                          ),
+                          child: Text(
+                            'FLUTTER',
+                            style: TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue[800],
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: flutter
+                              .map(
+                                (item) => Container(
+                                  padding: EdgeInsets.all(5),
+                                  decoration: BoxDecoration(
+                                    color: Colors.lightBlue[50],
+                                  ),
+                                  child: Text(
+                                    item,
+                                    style: TextStyle(
+                                      fontSize: 10,
+                                      color: Colors.blue[800],
+                                    ),
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        )
+                      ],
+                    ),
+                  )
+                : Container(),
+            dart.length > 0
+                ? Container(
+                    margin: EdgeInsets.only(top: 5),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: Colors.lightBlue[50],
+                            border: Border(
+                              right: BorderSide(
+                                width: 0.5,
+                                color: Colors.blue[800],
+                              ),
+                            ),
+                          ),
+                          child: Text(
+                            'DART',
+                            style: TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue[800],
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: dart
+                              .map(
+                                (item) => Container(
+                                  padding: EdgeInsets.all(5),
+                                  decoration: BoxDecoration(
+                                    color: Colors.lightBlue[50],
+                                  ),
+                                  child: Text(
+                                    item,
+                                    style: TextStyle(
+                                      fontSize: 10,
+                                      color: Colors.blue[800],
+                                    ),
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        )
+                      ],
+                    ),
+                  )
+                : Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/package/package.dart
+++ lib/pages/package/package.dart
@@ -1,22 +1,145 @@
 import 'package:flutter/material.dart';
+import 'dart:convert' show json;
+import 'package:flutter_show/components/language_change.dart';
+import 'package:flutter_show/components/package_cell.dart';
 
-class PackagePage extends StatelessWidget {
-  // This widget is the root of your application.
+class PackagePage extends StatefulWidget {
+  @override
+  _PackagePageState createState() => _PackagePageState();
+}
+
+class _PackagePageState extends State<PackagePage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Package'),
-        centerTitle: true,
-        leading: IconButton(
-            icon: Icon(Icons.dehaze),
-            onPressed: () {
-              Scaffold.of(context).openDrawer();
-            }),
-      ),
-      body: Center(
-        child: Text('PackagePage'),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            elevation: 0,
+            expandedHeight: 200,
+            leading: IconButton(
+              icon: Icon(Icons.dehaze),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            ),
+            actions: <Widget>[
+              LanguageChange(),
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              title: Image(
+                height: 30,
+                image: AssetImage('assets/images/package/pub-dev-logo.png'),
+              ),
+              background: Image(
+                image: AssetImage('assets/images/package/hero-bg-static.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: StickyTabBarDelegate(
+              child: TabBar(
+                labelPadding: EdgeInsets.zero,
+                labelColor: Colors.blue,
+                unselectedLabelColor: Colors.grey,
+                controller: _tabController,
+                tabs: <Widget>[
+                  Container(
+                    width: double.infinity,
+                    color: Colors.white,
+                    child: Tab(text: 'Basics'),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    color: Colors.white,
+                    child: Tab(text: 'UI'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverFillRemaining(
+            child: TabBarView(
+              controller: _tabController,
+              children: <Widget>[
+                FutureBuilder(
+                  future: DefaultAssetBundle.of(context)
+                      .loadString("assets/data/package/basics.json"),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      List<dynamic> data = json.decode(snapshot.data);
+                      return SingleChildScrollView(
+                        child: Column(
+                          children: data
+                              .map(
+                                (item) => PackageCell(
+                                  title: item['title'],
+                                  introductionEN: item['introductionEN'],
+                                  introductionCN: item['introductionCN'],
+                                  routeName: item['routeName'],
+                                  isNullSafety: item['isNullSafety'],
+                                  isFavourite: item['isFavourite'],
+                                  owner: item['owner'],
+                                  ownerPath: item['ownerPath'],
+                                  detailPath: item['detailPath'],
+                                  flutter: item['flutter'],
+                                  dart: item['dart'],
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      );
+                    }
+                    return CircularProgressIndicator();
+                  },
+                ),
+                Center(child: Text('Content of UI')),
+              ],
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class StickyTabBarDelegate extends SliverPersistentHeaderDelegate {
+  final TabBar child;
+
+  StickyTabBarDelegate({@required this.child});
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return this.child;
+  }
+
+  @override
+  double get maxExtent => this.child.preferredSize.height;
+
+  @override
+  double get minExtent => this.child.preferredSize.height;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
   }
 }
